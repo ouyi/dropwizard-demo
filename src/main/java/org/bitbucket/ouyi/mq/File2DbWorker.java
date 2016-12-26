0356322dd5d4fcdc5bf9b352967af279f8430e0e
Yi Ou <ouyi@gmx.de>
2016-12-26 17:18:53 +0000

    Add post request invocation

--- src/main/java/org/bitbucket/ouyi/mq/File2DbWorker.java
+++ src/main/java/org/bitbucket/ouyi/mq/File2DbWorker.java
@@ -7,6 +7,11 @@ import com.rabbitmq.client.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
 import java.io.IOException;
 
 public class File2DbWorker {
@@ -16,21 +21,27 @@ public class File2DbWorker {
     public static void main(String[] argv) throws Exception {
 
         WorkQueueSubscriber subscriber = new MessageQueueFactory().createSubscriber();
-
-        LOGGER.info(" [*] Waiting for messages. To exit press CTRL+C");
-
         Channel channel = subscriber.getChannel();
 
-        Consumer consumer = new DefaultConsumer(channel) {
-            @Override
-            public void handleDelivery(String consumerTag, Envelope envelope,
-                                       AMQP.BasicProperties properties, byte[] body) throws IOException {
-                String message = new String(body, "UTF-8");
-                LOGGER.info(" [x] Received '" + message + "'");
-                channel.basicAck(envelope.getDeliveryTag(), false);
-            }
-        };
+        Client client = ClientBuilder.newClient();
+        WebTarget webTarget = client.target("http://localhost:8080/transform");
+
+        Consumer consumer = createConsumer(channel, webTarget);
 
         subscriber.subscribe(consumer);
+    }
+
+    private static Consumer createConsumer(final Channel channel, final WebTarget webTarget) {
+        return new DefaultConsumer(channel) {
+                @Override
+                public void handleDelivery(String consumerTag, Envelope envelope,
+                                           AMQP.BasicProperties properties, byte[] body) throws IOException {
+                    String message = new String(body, "UTF-8");
+                    LOGGER.info("Received '" + message + "'");
+
+                    webTarget.path(message).request().post(Entity.entity(null, MediaType.WILDCARD_TYPE));
+                    channel.basicAck(envelope.getDeliveryTag(), false);
+                }
+            };
     }
 }
